9302b3a50a78218f7f0cb6207d55e0ae498cfe8e
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 15:21:57 +0800

    Create 3.php

---
+++ api/3.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$url = 'http://lige.unaux.com/?url=http://%E9%A5%AD%E5%A4%AA%E7%A1%AC.top/tv&i=1';
+//$url = iconv("gb2312", "utf-8//IGNORE",$url);
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+?>
