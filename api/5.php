1a27aecc09cda4c8043c76f046893b234715aea6
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 02:35:24 +0800

    Update 5.php

--- api/5.php
+++ api/5.php
@@ -1,12 +1,12 @@
 <?php
 //* This will give an error. Note the output
 // * above, which is before the header() call */
-header('Location: https://www.xn--sss604efuw.top/');
+header('Location: http://饭太硬.top/tv');
 exit;
 ?>
 
 //<?php  
-//$url = "https://www.xn--sss604efuw.top/";  
+//$url = "http://饭太硬.top/tv";  
 //echo "<script language= 'javascript' type='text/javascript'>";  
 //echo "window.location.href='$url'";  
 //echo "</script>";  
