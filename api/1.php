6ced758e8d4c2eea985972ef86c319b841ed3775
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 03:07:13 +0800

    Update 1.php

--- api/1.php
+++ api/1.php
@@ -1,5 +1,5 @@
 <?php
-$handle = fopen ("https://t4vod.hz.cz/api/pz?url=http://饭太硬.top/tv", "rb");
+$handle = fopen ("http://饭太硬.top/tv", "rb");
 $contents = "";
 do {
 $data = fread($handle, 1024);
