ad390218a07889440f3851be2ce250470cc732c0
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 14:55:47 +0800

    Update 2.php

--- api/2.php
+++ api/2.php
@@ -1,8 +1,7 @@
 <?php
-header("Content-Type:text/html;charset=utf-8");
-$keyworld="https://pastebin.com/raw/5NHaxyGR"; 
-$keyworld=iconv("utf-8","gb2312",$keyworld);
-$url = "http://lige.unaux.com/?url=$keyworld";
-$html = file_get_contents($url);
-$html = iconv("gb2312", "utf-8//IGNORE",$html);
-echo $html;
+header("Content-Type: text/html;charset=utf-8");
+$url = 'https://pastebin.com/raw/5NHaxyGR';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+$str = htmlspecialchars($str);//转换为源代码
+echo $str;
+?>
