b7828afd27681116c5caffdabe1ea4f16ac6f87e
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 02:20:05 +0800

    Update 5.php

--- api/5.php
+++ api/5.php
@@ -3,5 +3,6 @@ $url = "http://饭太硬.top/tv";
 echo "
 <script language= 'javascript' type='text/javascript'>";  
 echo "window.location.href='$url'";  
-echo "< /script>";  
+echo "
+</script>";  
 ?> 
