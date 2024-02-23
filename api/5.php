17ebf5f3cdd19ec8adba3b3c91efaea4443d6a07
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 02:28:04 +0800

    Update 5.php

--- api/5.php
+++ api/5.php
@@ -1,6 +1,6 @@
 <?php
-/* This will give an error. Note the output
- * above, which is before the header() call */
+//* This will give an error. Note the output
+// * above, which is before the header() call */
 header('Location: http://饭太硬.top/tv');
 exit;
 ?>
