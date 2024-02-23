b74bdca9bf62fe048b20a5bf70717809e804faa8
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 14:50:03 +0800

    Create 2.php

---
+++ api/2.php
@@ -0,0 +1,9 @@
+
+<?php
+header("Content-Type:text/html;charset=utf-8");
+$keyworld="https://pastebin.com/raw/5NHaxyGR"; 
+$keyworld=iconv("utf-8","gb2312",$keyworld);
+$url = "http://lige.unaux.com/?url=$keyworld";
+$html = file_get_contents($url);
+$html = iconv("gb2312", "utf-8//IGNORE",$html);
+echo $html;
