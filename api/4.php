e7e13dd66f55e781594b030c88ff7d166c2e8ec8
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 00:21:44 +0800

    Update 4.php

--- api/4.php
+++ api/4.php
@@ -1,14 +1,8 @@
 <?php
-$url = "https://www.xn--sss604efuw.top/jm/jiemi.php?url=https://agit.ai/leevi/PiG/raw/branch/master/jsm.json";
-$ch = curl_init();
-$timeout = 5;
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-//在需要用户检测的网页里需要增加下面两行
-//curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-//curl_setopt($ch, CURLOPT_USERPWD, US_NAME.":".US_PWD);
-$contents = curl_exec($ch);
-curl_close($ch);
-echo $contents;
+header("Content-Type: text/html;charset=utf-8");
+$url = 'http://lige.unaux.com/?url=http://%E9%A5%AD%E5%A4%AA%E7%A1%AC.top/tv';
+//$url = iconv("gb2312", "utf-8//IGNORE",$url);
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
 ?>
