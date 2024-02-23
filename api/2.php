927ca7fcda1f0b651a9b7f0b47d160b374e7bacd
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 15:11:18 +0800

    Update 2.php

--- api/2.php
+++ api/2.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
 $url = 'https://pastebin.com/raw/5NHaxyGR';
+$str = iconv("gb2312", "utf-8//IGNORE",$str);
 $str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
 $str = iconv("gb2312", "utf-8//IGNORE",$str);
 $str = htmlspecialchars($str);//转换为源代码
