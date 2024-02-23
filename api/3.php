ca0d2a1352db694c9f7c04f1fbb3a5c82fef3ea3
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 15:54:34 +0800

    Update 3.php

--- api/3.php
+++ api/3.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-$url = 'http://%E9%A5%AD%E5%A4%AA%E7%A1%AC.top/tv';
+$url = 'https://www.xn--sss604efuw.top/jm/jiemi.php?url=https://agit.ai/leevi/PiG/raw/branch/master/jsm.json';
 //$url = iconv("gb2312", "utf-8//IGNORE",$url);
 $str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
 //$str = iconv("gb2312", "utf-8//IGNORE",$str);
