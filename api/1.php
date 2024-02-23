08ce99b53e786b322d8a4df2d36d92955077be62
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 13:36:50 +0800

    Update 1.php

--- api/1.php
+++ api/1.php
@@ -1,3 +1,6 @@
 <?php 
-header("location: http://饭太硬.top/tv"); 
+$url = "http://hfr1107.top"; 
+$contents = file_get_contents($url); 
+$getcontent = iconv("gb2312", "utf-8",$contents); 
+echo $contents; 
 ?> 
