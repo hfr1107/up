c0edbd7a0d45b2a2b668cdcc196976fa42a9f7d7
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 14:34:03 +0800

    Update 1.php

--- api/1.php
+++ api/1.php
@@ -1,33 +1,13 @@
 <?php
-$url='http://饭太硬.top/tv';
-if(!empty($t_url)) {
-preg_match('/(http|https):\/\//',$t_url,$matches);
-if($matches){
-$url=$t_url;
-} else {
-preg_match('/\./i',$t_url,$matche);
-if($matche){
-$url='http://'.$t_url;
-} else {
-$url='http://饭太硬.top/tv';
+$handle = fopen ("https://pastebin.com/raw/5NHaxyGR", "rb");
+$contents = "";
+do {
+$data = fread($handle, 1024);
+if (strlen($data) == 0) {
+break;
 }
-}
-} else {
-$url='https://pastebin.com/raw/5NHaxyGR';
-}
+$contents .= $data;
+} while(true);
+fclose ($handle);
+echo $contents;
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="5;url='<?php echo $url;?>';">
-<div id="circle"></div>
-<div id="circletext"></div>
-<div id="circle1"></div>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="1;url='<?php echo $url;?>';">
-</head>
-<body>
-</body>
-</html>
