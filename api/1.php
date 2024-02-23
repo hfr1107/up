b47e08b966f9d114fa15ced562ad7d4e3863fff5
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 14:17:40 +0800

    Update 1.php

--- api/1.php
+++ api/1.php
@@ -1,6 +1,33 @@
-<?php 
-$url = "http://hfr1107.top"; 
-$contents = file_get_contents($url); 
-$getcontent = iconv("gb2312", "utf-8",$contents); 
-echo $contents; 
-?> 
+<?php
+$t_url=$_GET['url'];
+if(!empty($t_url)) {
+preg_match('/(http|https):\/\//',$t_url,$matches);
+if($matches){
+$url=$t_url;
+} else {
+preg_match('/\./i',$t_url,$matche);
+if($matche){
+$url='http://'.$t_url;
+} else {
+$url='http://饭太硬.top/tv';
+}
+}
+} else {
+$url='https://pastebin.com/raw/5NHaxyGR';
+}
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="refresh" content="5;url='<?php echo $url;?>';">
+<div id="circle"></div>
+<div id="circletext"></div>
+<div id="circle1"></div>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="refresh" content="1;url='<?php echo $url;?>';">
+</head>
+<body>
+</body>
+</html>
