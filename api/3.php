3a809bb5929ea9a6e7d4a0371a7aff094f2ff083
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-23 23:12:57 +0800

    Update 3.php

--- api/3.php
+++ api/3.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-$url = 'https://www.xn--sss604efuw.top/jm/jiemi.php?url=https://agit.ai/leevi/PiG/raw/branch/master/jsm.json';
+$url = 'https://www.xn--sss604efuw.top/jm/jiemi.php?url=http://饭太硬.top/tv';
 //$url = iconv("gb2312", "utf-8//IGNORE",$url);
 $str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
 //$str = iconv("gb2312", "utf-8//IGNORE",$str);
