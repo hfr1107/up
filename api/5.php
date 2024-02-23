088be0f6bf71626fe37e7d220e32aeb962e62313
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 02:59:14 +0800

    Update 5.php

--- api/5.php
+++ api/5.php
@@ -1,24 +1,3 @@
-//*<?php
-//* This will give an error. Note the output
-// * above, which is before the header() call */
-//header('Location: http://肥猫.live');
-//exit;
-//?>
-
-//*<?php  
-//$url = "http://肥猫.live";  
-//echo "<script language= 'javascript' type='text/javascript'>";  
-//echo "window.location.href='$url'";  
-//echo "</script>";  
-//?> 
-
-//*<?php  
-//$url = "http://肥猫.live";  
-//echo "<script type='text/javascript'>";  
-//echo "window.location.replace='$url'";  
-//echo "</script>";  
-//?> 
-
 <?php  
 $url = "http://肥猫.live";  
 echo "";  
