fef638674004cf2020b80d6b5ccd300a03f67cf6
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-02-24 02:24:39 +0800

    Update 5.php

--- api/5.php
+++ api/5.php
@@ -1,6 +1,14 @@
-<?php  
-$url = "http://饭太硬.top/tv";  
-echo "<script language= 'javascript' type='text/javascript'>";  
-echo "window.location.href='$url'";  
-echo "</script>";  
-?> 
+<html>
+<?php
+/* This will give an error. Note the output
+ * above, which is before the header() call */
+header('Location: http://饭太硬.top/tv');
+exit;
+?>
+
+//<?php  
+//$url = "http://饭太硬.top/tv";  
+//echo "<script language= 'javascript' type='text/javascript'>";  
+//echo "window.location.href='$url'";  
+//echo "</script>";  
+//?> 
