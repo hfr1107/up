9f32d1c3f0d26573d3a6b915b612702e93e3b329
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-03 00:47:17 +0800

    Create dc.php

---
+++ api/dc.php
@@ -0,0 +1,13 @@
+<?php
+$handle = fopen ("https://hfr1107.github.io/up/dc.json", "rb");
+$contents = "";
+do {
+$data = fread($handle, 1024);
+if (strlen($data) == 0) {
+break;
+}
+$contents .= $data;
+} while(true);
+fclose ($handle);
+echo $contents;
+?>
