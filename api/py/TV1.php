1859506793c9c7a809997ea3eb09e01fcde52e74
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-19 03:08:48 +0800

    Create TV1.php

---
+++ api/py/TV1.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$url = 'https://hfr1107.github.io/up/tv/tv1.txt';
+//$url = iconv("gb2312", "utf-8//IGNORE",$url);
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+?>
