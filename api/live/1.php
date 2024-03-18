839215877e9e5b1304d6216fe14ae9bb031d9666
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-19 02:59:35 +0800

    Create 1.php

---
+++ api/live/1.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$url = 'https://t4vod.hz.cz/api/pz?url=http://饭太硬.top/tv';
+//$url = iconv("gb2312", "utf-8//IGNORE",$url);
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+?>
