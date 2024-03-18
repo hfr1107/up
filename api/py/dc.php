e0fdaaf45b73b2f77d267a15a291457e03a2aba7
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-19 03:36:52 +0800

    Update dc.php

--- api/py/dc.php
+++ api/py/dc.php
@@ -1,8 +1,13 @@
 <?php
-header("Content-Type: text/html;charset=utf-8");
-$url = 'https://hfr1107.github.io/dc.json';
-//$url = iconv("gb2312", "utf-8//IGNORE",$url);
-$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
-//$str = iconv("gb2312", "utf-8//IGNORE",$str);
-echo $str;
+$handle = fopen ("https://hfr1107.github.io/up/dc.json", "rb");
+$contents = "";
+do {
+$data = fread($handle, 1024);
+if (strlen($data) == 0) {
+break;
+}
+$contents .= $data;
+} while(true);
+fclose ($handle);
+echo $contents;
 ?>
