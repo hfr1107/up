ce5102f010c35bf6454c62454d468e1517074931
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-22 03:37:03 +0800

    Create cs.php

---
+++ api/cs/cs.php
@@ -0,0 +1,14 @@
+<?php 
+//JSRUN引擎2.0，支持多达30种语言在线运行，全仿真在线交互输入输出。 
+// 假设JSON文件名为"data.json"
+$jsonFile = "https://hfr1107.github.io/gao/9918.json";
+ 
+
+// 读取文件内容
+$jsonData = file_get_contents($jsonFile);
+ 
+// 将JSON字符串转换为PHP对象
+$dataObject = json_decode($jsonData);
+ 
+// 使用对象中的数据
+print_r($dataObject);
