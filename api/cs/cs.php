e0117768fd261ec2f3120cb8c3d40171c281fd37
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-23 01:39:14 +0800

    Update cs.php

--- api/cs/cs.php
+++ api/cs/cs.php
@@ -1,52 +1,90 @@
-<?php 
-//JSRUN引擎2.0，支持多达30种语言在线运行，全仿真在线交互输入输出。 
-// 假设JSON文件名为"data.json"
+
 <?php
-echo "跳转前的代码内容";
+header("Content-Type: text/html;charset=utf-8");
+$p = isset($_GET['p']) ? $_GET['p'] : '';
+$v = isset($_GET['v']) ? $_GET['v'] : '';
  
-// 页面跳转
-header("Location:https://xn--sss604efuw.top/tv");
-exit; // 确保之后的代码不会执行
+switch ($p) {
+    case 'live':
+        // 根据$v的值返回对应数据
+        switch ($v) {
+            case '1':
+$url = 'https://hfr1107.github.io/up/tv/tv.txt';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            case '2':
+$url = 'https://hfr1107.github.io/up/tv/tv1.txt';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            case '3':
+$url = 'https://hfr1107.github.io/up/tv/tv2.txt';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            default:
+        echo '未知参数';
+                break;
+        }
+        break;
 
+    case 'app':
+        // 根据$v的值返回对应数据
+        switch ($v) {
+            case '1':
+$url = 'https://hfr1107.github.io/up/appmarket/ads.php';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            case '2':
+$url = 'https://hfr1107.github.io/up/appmarket/index.php';
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            default:
+$url = '';
+                break;
+        }
+        break;
 
-$jsonFile = "https://xn--sss604efuw.top/tv";
- 
+switch ($p) {
+    case 'tv':
+        // 根据$v的值返回对应数据
+        switch ($v) {
+            case '0':
+$handle = fopen ("https://hfr1107.github.io/up/dc.json", "rb");
+$contents = "";
+do {
+$data = fread($handle, 1024);
+if (strlen($data) == 0) {
+break;
+}
+$contents .= $data;
+} while(true);
+fclose ($handle);
+echo $contents;
+                break;
+            case '1':
+$url = 'https://t4vod.hz.cz/api/pz?url=http://饭太硬.top/tv';
+//$url = iconv("gb2312", "utf-8//IGNORE",$url);
+$str = file_get_contents($url);//获取网页，此时输出$str为解析版网页
+//$str = iconv("gb2312", "utf-8//IGNORE",$str);
+echo $str;
+                break;
+            default:
+        echo '未知参数';
+                break;
+        }
+        break;
 
-// 读取文件内容
-$jsonData = file_get_contents($jsonFile);
-
-// 读取文件内容
-$jsonData = file_get_contents($jsonFile);
-
-echo $jsonData;
-// 将JSON字符串转换为PHP数组
-$dataArray = json_decode($jsonData, true);
- 
-// 使用数组中的数据
-print_r($dataArray);
-
-$jsonFile = "https://xn--sss604efuw.top/tv";
-
-// 将JSON字符串转换为PHP对象
-$dataObject = json_decode($jsonData);
- 
-// 使用对象中的数据
-print_r($dataObject);
- 
-// 使用正则表达式匹配隐藏的JSON数据
-preg_match_all('/<!--([^{}]*?)-->/', "https://xn--sss604efuw.top/tv", $matches);
- 
-// 提取并解码JSON数据
-$jsonData = array_map(function($match) {
-    return json_decode($match, true);
-}, $matches[1]);
- 
-// 输出结果
-print_r($jsonData);
-
-// 读取文件内容
-$jsonData = file_get_contents($jsonFile);
-
-echo $jsonData;
-
-
+    default:
+        echo '未知参数';
+        break;
+}
+?>
