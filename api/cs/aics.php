4529691db4cb31a8fe16cea22a5bcce4e707b327
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2024-03-27 03:42:20 +0800

    Update aics.php

--- api/cs/aics.php
+++ api/cs/aics.php
@@ -2,15 +2,15 @@
 
 $p = isset($_GET['p']) ? $_GET['p'] : '';
 $v = isset($_GET['v']) ? $_GET['v'] : '';
+$maxRetries = 1; 
 $retries = 0; // 初始化 retries 变量
-$maxRetries = 3; 
 function fetchContent($url, &$retries,  $maxRetries) { // 通过引用传递 retries 变量，并添加 maxRetries 参数
     $context = stream_context_create([
         'http' => [
             'timeout' => 5
         ]
     ]);
-    $content = file_get_contents($url, false, $context);
+    $content = @file_get_contents($url, false, $context);
     if ($content === FALSE) {
  if ($retries < $maxRetries) { 
             sleep(1); 
